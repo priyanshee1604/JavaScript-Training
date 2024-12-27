0dd00f5ef05ae3f875be32c06a39e0ead24f3cd4
priyanshee1604 <priyansheesisodiya1604@gmail.com>
2024-12-27 13:56:44 +0530

    forms

---
+++ personal/forms/script.js
@@ -0,0 +1,24 @@
+//by default when submitting forms the page refreshes thus we need to handle it in some way.
+//using formData object in JS.
+
+let form = document.forms; //this returns an array of forms. if not use getElementById
+console.log(form);
+
+form[0].addEventListener("submit", (e) => {
+    e.preventDefault();
+});
+
+//used to create form with already a given data. used to send HTTP requests.
+// let formdata  = new FormData('form');
+// console.log(formdata);
+
+//another way to access the form
+// let formm = document.forms.form;
+// console.log(formm);
+
+//accessing the elements
+let username = form[0].elements['username'];
+console.log(username.value);
+console.log(form[0].password);
+console.log(form[0].elements['testing']);
+console.log(form[0].elements['radiotest']);
