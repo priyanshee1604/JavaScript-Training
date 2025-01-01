5bda56dec0f1e38ac6752adf7ebb76ba55e954e0
priyanshee1604 <priyansheesisodiya1604@gmail.com>
2025-01-01 20:25:28 +0530

    some changes

--- module4/question3.js
+++ module4/question3.js
@@ -2,7 +2,7 @@
 const array = [1,2,3,4,5,6,7,7,89,10];
 function sum(array, n){
     // console.log(array[n]);
-    if(n == 0)
+    if(n < 0)
         return 0;
     else 
         return array[n] + sum(array, n-1);
---
+++ personal/function/fun1.js
@@ -0,0 +1,6 @@
+function fun(a,b,a){
+    console.log(a);
+    console.log(b);
+    console.log(a);
+}
+fun(1,2,3);
